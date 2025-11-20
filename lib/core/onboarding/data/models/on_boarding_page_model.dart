fb76355aee81c854f8304f192116d395a409ce88
Mustafa Elbaz <m9stafa05@gmail.com>
2025-11-20 16:53:49 +0200

    Add onboarding page model and implement onboarding screen with navigation

---
+++ lib/core/onboarding/data/models/on_boarding_page_model.dart
@@ -0,0 +1,11 @@
+class OnBoardingPageModel {
+  final String image;
+  final String title;
+  final String subtitle;
+
+  OnBoardingPageModel({
+    required this.image,
+    required this.title,
+    required this.subtitle,
+  });
+}
