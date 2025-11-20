25c9c3d9f16f665c3d46c981d7671e7804749996
Mustafa Elbaz <m9stafa05@gmail.com>
2025-11-20 18:46:30 +0200

    Add CustomTextFormField widget and update text styles for consistency

--- lib/core/themes/app_text_styles.dart
+++ lib/core/themes/app_text_styles.dart
@@ -14,9 +14,34 @@ class AppTextStyles {
     fontWeight: FontWeightHelper.regular,
     color: AppColors.grey,
   );
+  static TextStyle font16GreyRegular = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeightHelper.regular,
+    color: AppColors.grey,
+  );
   static TextStyle font16WhiteRegular = const TextStyle(
     fontSize: 16,
     fontWeight: FontWeightHelper.regular,
     color: Colors.white,
+  );
+  static TextStyle font18WhiteRegular = const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeightHelper.semiBold,
+    color: Colors.white,
+  );
+  static TextStyle font16BlackRegular = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeightHelper.regular,
+    color: Colors.black,
+  );
+  static TextStyle font16greyRegular = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeightHelper.regular,
+    color: AppColors.grey,
+  );
+  static TextStyle font16BlackBold = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeightHelper.bold,
+    color: Colors.black,
   );
 }
