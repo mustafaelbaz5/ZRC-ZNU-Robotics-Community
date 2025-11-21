f86284b96d710b8ec9025b7b0dbf8ec6d17e7b0c
Mustafa Elbaz <m9stafa05@gmail.com>
2025-11-21 17:43:04 +0200

    Update SecureStorage Methods  to be future

--- lib/core/utils/secure_storage.dart
+++ lib/core/utils/secure_storage.dart
@@ -3,12 +3,12 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class SecureStorage {
   final _storage = const FlutterSecureStorage();
 
-  void saveString({required String key, required String value}) {
-    _storage.write(key: key, value: value);
+  Future<void> saveString({required String key, required String value}) async {
+    await _storage.write(key: key, value: value);
   }
 
-  Future<String?> getString({required String key}) {
-    return _storage.read(key: key);
+  Future<String?> getString({required String key}) async {
+    return await _storage.read(key: key);
   }
 
   Future<void> clear({required String key}) async {
