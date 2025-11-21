0b73d3fbd5b406884bfa6737e8280fdc82779ee5
Mustafa Elbaz <m9stafa05@gmail.com>
2025-11-22 00:56:33 +0200

    Build Home Screen layout with Categories and Courses sections

--- lib/modules/student/features/home/ui/home_screen.dart
+++ lib/modules/student/features/home/ui/home_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:zrc/core/themes/app_colors.dart';
+import 'package:zrc/core/themes/app_text_styles.dart';
+import 'package:zrc/core/utils/spacing.dart';
+import 'package:zrc/modules/student/features/home/ui/widgets/categories_list_view.dart';
+import 'package:zrc/modules/student/features/home/ui/widgets/courses_list_view.dart';
+import 'package:zrc/modules/student/features/home/ui/widgets/home_app_bar.dart';
+import 'package:zrc/modules/student/features/home/ui/widgets/home_header.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +13,44 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Container(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              verticalSpacing(20),
+              const HomeAppBar(userName: 'Mustafa', userId: '51546'),
+              verticalSpacing(20),
+              const HomeHeader(),
+              verticalSpacing(20),
+              Text("Categories", style: AppTextStyles.font16BlackBold),
+              verticalSpacing(12),
+              const CategoriesListView(),
+              Row(
+                children: [
+                  Text("Courses", style: AppTextStyles.font16BlackBold),
+                  const Spacer(),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text("See all", style: AppTextStyles.font13BlueBold),
+                  ),
+                  horizontalSpacing(4),
+                  const Icon(
+                    Icons.arrow_forward_ios,
+                    color: AppColors.lightBlue,
+                    size: 16,
+                  ),
+                  horizontalSpacing(10),
+                ],
+              ),
+
+              const CoursesListView(), //
+              verticalSpacing(20),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
