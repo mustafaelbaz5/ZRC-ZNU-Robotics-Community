5280605b2c2aa0cff93758eefd99ded2d5461493
Mustafa Elbaz <m9stafa05@gmail.com>
2025-11-22 00:49:24 +0200

    Build Category Section

---
+++ lib/modules/student/features/home/ui/widgets/categories_list_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:zrc/modules/student/features/home/data/model/category_model.dart';
+import 'package:zrc/modules/student/features/home/ui/widgets/categories_list_view_item.dart';
+
+class CategoriesListView extends StatefulWidget {
+  const CategoriesListView({super.key});
+
+  @override
+  State<CategoriesListView> createState() => _CategoriesListViewState();
+}
+
+class _CategoriesListViewState extends State<CategoriesListView> {
+  int selectedIndex = 0;
+  final categories = CategoryModel.categories;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100.h,
+      child: ListView.builder(
+        itemCount: categories.length,
+        scrollDirection: Axis.horizontal,
+        physics: const BouncingScrollPhysics(),
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return CategoriesListViewItem(
+            isSelected: selectedIndex == index,
+            onTap: () {
+              setState(() => selectedIndex = index);
+            },
+            icon: categories[index].icon,
+            title: categories[index].title,
+          );
+        },
+      ),
+    );
+  }
+}
