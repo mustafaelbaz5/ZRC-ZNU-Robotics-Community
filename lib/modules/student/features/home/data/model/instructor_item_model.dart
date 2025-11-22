421d330ec5a1dea47d24bfff5de4a8b01bc473d6
Mustafa Elbaz <m9stafa05@gmail.com>
2025-11-22 21:09:10 +0200

    feat: Implement Instructors List View with dynamic data model

---
+++ lib/modules/student/features/home/data/model/instructor_item_model.dart
@@ -0,0 +1,7 @@
+class InstructorItemModel {
+  final int id;
+  final String name;
+  final String image;
+
+  InstructorItemModel( { required this.id, required this.name, required this.image});
+}
--- lib/modules/student/features/home/ui/home_screen.dart
+++ lib/modules/student/features/home/ui/home_screen.dart
@@ -1,52 +1,65 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:zrc/core/themes/app_colors.dart';
-import 'package:zrc/core/themes/app_text_styles.dart';
-import 'package:zrc/core/utils/spacing.dart';
-import 'package:zrc/modules/student/features/home/ui/widgets/categories_list_view.dart';
-import 'package:zrc/modules/student/features/home/ui/widgets/courses_list_view.dart';
-import 'package:zrc/modules/student/features/home/ui/widgets/home_app_bar.dart';
-import 'package:zrc/modules/student/features/home/ui/widgets/home_header.dart';
+import 'package:zrc/modules/student/features/home/ui/widgets/instructors_list_view.dart';
+
+import '../../../../../core/themes/app_colors.dart';
+import '../../../../../core/themes/app_text_styles.dart';
+import '../../../../../core/utils/spacing.dart';
+import 'widgets/categories_list_view.dart';
+import 'widgets/courses_list_view.dart';
+import 'widgets/home_app_bar.dart';
+import 'widgets/home_header.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
+  const HomeScreen({super.key, required this.navigationKey});
+  final GlobalKey<CurvedNavigationBarState> navigationKey;
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            verticalSpacing(20),
-            const HomeAppBar(userName: 'Mustafa', userId: '51546'),
-            verticalSpacing(20),
-            const HomeHeader(),
-            verticalSpacing(20),
-            Text("Categories", style: AppTextStyles.font16BlackBold),
-            verticalSpacing(12),
-            const CategoriesListView(),
-            Row(
-              children: [
-                Text("Courses", style: AppTextStyles.font16BlackBold),
-                const Spacer(),
-                GestureDetector(
-                  onTap: () {},
-                  child: Text("See all", style: AppTextStyles.font13BlueBold),
-                ),
-                horizontalSpacing(4),
-                const Icon(
-                  Icons.arrow_forward_ios,
-                  color: AppColors.lightBlue,
-                  size: 16,
-                ),
-                horizontalSpacing(10),
-              ],
-            ),
-
-            const CoursesListView(), //
-            verticalSpacing(20),
-          ],
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              verticalSpacing(20),
+              const HomeAppBar(userName: 'Mustafa', userId: '51546'),
+              verticalSpacing(20),
+              HomeHeader(navigationKey: navigationKey),
+              verticalSpacing(24),
+              Text("Categories", style: AppTextStyles.font16BlackBold),
+              verticalSpacing(12),
+              const CategoriesListView(),
+              verticalSpacing(24),
+              Row(
+                children: [
+                  Text("Courses", style: AppTextStyles.font16BlackBold),
+                  const Spacer(),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Row(
+                      children: [
+                        Text("See all", style: AppTextStyles.font13BlueBold),
+                        horizontalSpacing(4),
+                        const Icon(
+                          Icons.arrow_forward_ios,
+                          color: AppColors.lightBlue,
+                          size: 16,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              verticalSpacing(12),
+              const CoursesListView(),
+              verticalSpacing(24),
+              Text("Top Instructors", style: AppTextStyles.font16BlackBold),
+              verticalSpacing(12),
+              const InstructorsListView(),
+              verticalSpacing(30),
+            ],
+          ),
         ),
       ),
     );
